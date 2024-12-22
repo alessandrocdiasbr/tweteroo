286244112743bb269bbaa9e37ca9751f76dae51b
alessandrocdiasbr <acdias83@gmail.com>
2024-12-22 14:44:42 -0300

    Rota de cadastro de usuário criado

--- src/app.js
+++ src/app.js
@@ -11,19 +11,37 @@ app.use(cors());
 app.use(json()); 
 
 const mongoURL = process.env.BACKEND_URL;
-const mongoClientUsers = new MongoClient(mongoURL);
+const mongoClient = new MongoClient(mongoURL);
 let db;
 
-mongoClientUsers.connect()
+mongoClient.connect()
 .then(() => {
     console.log("Conexão com o banco de dados estabelecida com sucesso!");
-    db = mongoClientUsers.db();
+   db = mongoClient.db();
 
 })
 .catch((err) => console.log(err.message));
 
+app.post("/users", async (req, res) => {
+    const schema = joi.object({
+        username: joi.string().required(),
+        avatar: joi.string().uri().required(),
+    });
 
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(422).send(error.details[0].message);
+    }
 
+    const user = req.body;
+    try {
+        const result = await db.collection("users").insertOne(user);
+        res.status(201).send({ _id: result.insertedId, ...user });
+    } catch (err) {
+        console.error("Erro ao cadastrar usuário:", err); 
+        res.status(500).send("Erro ao cadastrar usuário");
+    }
+});
 
 
 
